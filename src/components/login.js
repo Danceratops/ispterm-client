4d2a4a70550e65bd18348045602e7321d6f14270
Danielle Reed <dan.reed.993@gmail.com>
2020-11-12 19:16:48 -0500

    inital commit

---
+++ src/components/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+import '../styles/imports.scss'
+
+const Login = ({children}) => {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+export default Login;
