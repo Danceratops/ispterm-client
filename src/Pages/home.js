30cb21741979e77b41387181fd9e32de28c36a83
Danielle Reed <dan.reed.993@gmail.com>
2020-11-17 20:23:53 -0500

    Add hero component

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import Hero from './../components/hero';
+
+import heroBackground from './../images/heroBackground.jpg';
+
 const home = () => {
     return (
         <div class="home">
-            <h1>Home Page</h1>
+            <Hero title="Homegrown Herbs" subtitle="Perfect for any occasion" backgroundImg={heroBackground}>
+                <p>Start shopping now&hellip;</p>
+            </Hero>
         </div>
     )
 }
