37b35b3d3c3e18617d714b1e41ac1d0372d82042
sgun7 <srg97@zips.uakron.edu>
2020-11-19 15:07:32 -0500

    not fixed

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from "prop-types";
+//import Background from '/herbsIcon.svg'
 
 import '../styles/imports.scss'
 
@@ -8,10 +9,14 @@ const Footer= () => {
         <div className="main-footer">
             <div className="box">
                     <div className="column">
+                    <img src={process.env.PUBLIC_URL + '/herbsIcon.svg'} /> 
                        <p1 className="footer"> &copy;{new Date().getFullYear()} ISP Term Project | All rights reserved | Terms of Service | Privacy  <br/> Stephan Gunawardana Danielle Reed Douglas Rudy</p1>
+                    </div>
+                    <div className="background">
                     </div>
             </div>
         </div>
+        
     )
 }
 
