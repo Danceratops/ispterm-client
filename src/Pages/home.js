7f05ef5a0f2a67fca45262b0957d8806572a5190
sgun7 <srg97@zips.uakron.edu>
2020-11-19 16:26:22 -0500

    Add in needed flexbox

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import Homecards from '../components/homecard';
-import Hero from '../components/hero';
+import React from "react";
+import Homecards from "../components/homeCard";
+import Hero from "../components/hero";
 
 const home = () => {
-    return (
-        <div class="home">
-            <h1>Home Page</h1>
-        
-            <Homecards/>
-        </div>
-    )
-}
+  return (
+    <div class="home">
+      <h1>Home Page</h1>
+
+      <Homecards />
+    </div>
+  );
+};
 
 export default home;
