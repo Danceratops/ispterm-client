747635e9df0878062a0011f28a25dc1a02a057fa
sgun7 <srg97@zips.uakron.edu>
2020-11-19 16:32:39 -0500

    add in rebase

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 
 import Hero from "./../components/hero";
@@ -67,3 +68,20 @@ const home = () => {
 };
 
 export default home;
+=======
+import React from 'react';
+import Homecards from '../components/homecard';
+import Hero from '../components/hero';
+
+const home = () => {
+    return (
+        <div class="home">
+            <h1>Home Page</h1>
+        
+            <Homecards/>
+        </div>
+    )
+}
+
+export default home;
+>>>>>>> ff1fe70... Start with flexbox
