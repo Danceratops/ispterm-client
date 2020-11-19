0171005d3a70b2e2c90f1ec4bde0c0af3c7b4c4c
sgun7 <srg97@zips.uakron.edu>
2020-11-19 15:35:12 -0500

    Add in merge

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import home from './Pages/home';
-import login from './Pages/login';
-import signup from './Pages/signup';
-import cart from './Pages/cart';
+import home from "./Pages/home";
+import login from "./Pages/login";
+import signup from "./Pages/signup";
+import cart from "./Pages/cart";
 
-import Nav from './components/navbar';
+import Nav from "./components/navbar";
 
-import './styles/imports.scss';
-import Footer from './components/footer'
-import Login from './components/login'
-import './styles/imports.scss'
+import "./styles/imports.scss";
+import Footer from "./components/footer";
+import Login from "./components/login";
+import "./styles/imports.scss";
 
 function App() {
   return (
@@ -18,14 +18,17 @@ function App() {
       <Router>
         <Nav />
         <Switch>
-          <Route exact path='/' component={home}/>
-          <Route exact path='/login' component={login}/>
-          <Route exact path='/signup' component={signup}/>
-          <Route exact path='/cart' component={cart}/>
+          <Route exact path="/" component={home} />
+          <Route exact path="/login" component={login} />
+          <Route exact path="/signup" component={signup} />
+          <Route exact path="/cart" component={cart} />
         </Switch>
       </Router>
-      <Login/>
-      <Footer/>
+      <h1>hello from outside login</h1>
+      <Login>
+        <h1>inside the login</h1>
+      </Login>
+      <Footer />
     </div>
   );
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
 //import Background from '/herbsIcon.svg'
 
-import '../styles/imports.scss'
+import "../styles/imports.scss";
 
-const Footer= () => {
-    return (
-        <div className="main-footer">
-            <div className="box">
-                    <div className="column">
-                    <img src={process.env.PUBLIC_URL + '/herbsIcon.svg'} /> 
-                       <p1 className="footer"> &copy;{new Date().getFullYear()} ISP Term Project | All rights reserved | Terms of Service | Privacy  <br/> Stephan Gunawardana Danielle Reed Douglas Rudy</p1>
-                    </div>
-                    <div className="background">
-                    </div>
-            </div>
+const Footer = () => {
+  return (
+    <div className="main-footer">
+      <div className="box">
+        <div className="column">
+          <img src={process.env.PUBLIC_URL + "/herbsIcon.svg"} />
+          <p1 className="footer">
+            {" "}
+            &copy;{new Date().getFullYear()} ISP Term Project | All rights
+            reserved | Terms of Service | Privacy <br /> Stephan Gunawardana
+            Danielle Reed Douglas Rudy
+          </p1>
         </div>
-        
-    )
-}
+        <div className="background"></div>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
