69c8b8d867547ba7bb5cdf03b543b2067d1c4611
sgun7 <srg97@zips.uakron.edu>
2020-11-22 16:51:09 -0500

    fix grid issues

--- src/Pages/products.js
+++ src/Pages/products.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import greenhouse from "./../images/greenhouse.jpg";
 
 const products = () => {
-    return (
-        <div class="home">
-            <h1>Products Page</h1>
-        </div>
-    )
-}
+  return (
+    <div class="home">
+      <Grid container>
+        <Grid
+          className="product-image"
+          item
+          xs={6}
+          container
+          direction="column"
+          justify="center"
+          alignItems="flex-end"
+        >
+          <img src={greenhouse} />
+        </Grid>
 
-export default products
+        <Grid
+          className="product-text"
+          item
+          xs={6}
+          container="row"
+          justify="flex-start"
+          alignItems="flex-start"
+        >
+          <h1 className="product-title">Parsley</h1>
+          <p1 className="product-price">Price</p1>
+          <h1 className="product-desc">hello</h1>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default products;
--- src/Pages/search.js
+++ src/Pages/search.js
@@ -1,29 +1,41 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
+import React from "react";
 
-import Card from '../components/card'
+import Card from "../components/card";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 
 const Search = () => {
-    const [products, setProducts] = useState(null);
-    useEffect(() => {
-        axios.get('/products').then((res) => {
-            console.log(res.data);
-            setProducts(res.data);
-        }).catch((err) => {
-            console.log(err);
-        })
-    }, []);
+  const [products, setProducts] = useState(null);
+  useEffect(() => {
+    axios
+      .get("/products")
+      .then((res) => {
+        console.log(res.data);
+        setProducts(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    var productMarkUp = products ? (
-    products.map(product => <Card key={product.productId} title={product.productId} price={product.productPrice} description={product.Description} imageSource={product.image} />)
-    ) : (<p>Loading...</p>)
+  var productMarkUp = products ? (
+    products.map((product) => (
+      <Card
+        key={product.productId}
+        title={product.productId}
+        price={product.productPrice}
+        description={product.Description}
+        imageSource={product.image}
+      />
+    ))
+  ) : (
+    <p>Loading...</p>
+  );
 
-    console.log(productMarkUp);
-    return (
-        <div className="search">
-            {productMarkUp}
-        </div>
-    )
-}
+  console.log(productMarkUp);
+  return <div className="search">{productMarkUp}</div>;
+};
 
-export default Search
+export default Search;
