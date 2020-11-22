8be5a6dbb211a466a2dfa9f49c110ca0d8faa9a9
sgun7 <srg97@zips.uakron.edu>
2020-11-22 16:51:09 -0500

    fix grid issues

--- src/Pages/search.js
+++ src/Pages/search.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import Card from '../components/card'
+import Card from "../components/card";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 
 const search = () => {
-    return (
-        <div class="home">
-            <h1>Search Page</h1>
-            <Card />
-        </div>
-    )
-}
+  return (
+    <div class="home">
+      <h1>Search Page</h1>
+      <Card />
+      <Button component={Link} to="/products">
+        Product Page
+      </Button>
+    </div>
+  );
+};
 
-export default search
+export default search;
