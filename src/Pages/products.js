b2eae3c6defc031671065c5b0a0fe5b2a76d662c
sgun7 <srg97@zips.uakron.edu>
2020-11-24 22:02:20 -0500

    Correct formatting

--- src/Pages/products.js
+++ src/Pages/products.js
@@ -1,44 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import greenhouse from "./../images/greenhouse.jpg";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
-import { IconButton } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
 
-const products = () => {
+const Products = () => {
+  const [quantity, setQuantity] = useState("");
   return (
     <div class="home">
-      <Grid className="product-grid-container" container spacing={4}>
-        <Grid className="product-picture" item xs={6}>
-          <img class="product-img" src={greenhouse}></img>
-        </Grid>
-        <Grid className="product-text" item xs={6}>
+      <div className="product-grid-container">
+        <div className="product-picture">
+          <img class="product-img" src={greenhouse} alt=" "></img>
+        </div>
+        <div className="product-title-price">
           <h1 className="product-title">Parsley</h1>
           <h2 className="product-price">Price</h2>
-          <p1 className="product-desc">
+          <p1>
             Parsley is widely used in Middle Eastern, Mediterranean, Brazilian,
             and American cuisine. Curly leaf parsley is used often as a garnish.
             Green parsley is used frequently as a garnish on potato dishes
             (boiled or mashed potatoes), on rice dishes (risotto or pilaf), on
             fish, fried chicken, lamb, goose, and steaks, as well in meat or
-            vegetable stews (including shrimp creole, beef bourguignon, goulash,
-            or chicken paprikash).
+            vegetable stews (i**ncluding shrimp creole, beef bourguignon,
+            goulash, or chicken paprikash).
           </p1>
-          <Grid className="product-box" item xs={9} container direction="row">
-            <h1 className="amount">Quantity: </h1>
-            <input
-              type="text"
-              className="quantity-input"
-              id="quantity"
-              size="4"
-            ></input>
-            <h1 className="pounds-total">lbs = Total: </h1>
-            <h1 className="product-output-price">$2.13</h1>
-            <ShoppingCartIcon />
-          </Grid>
-        </Grid>
-      </Grid>
+        </div>
+      </div>
+      <div className="product-box">
+        <TextField
+          type="text"
+          className="quantity-input"
+          id="quantity"
+          label="Quantity"
+          variant="outlined"
+          value={quantity}
+        />
+        <h1 className="pounds-total">lbs = Total: </h1>
+        <h1 className="product-output-price">$2.13</h1>
+        <ShoppingCartIcon className="product-icon" />
+      </div>
     </div>
   );
 };
 
-export default products;
+export default Products;
