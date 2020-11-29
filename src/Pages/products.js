a3e90c14acc20a6dffbd50949676aad9dfa33662
sgun7 <srg97@zips.uakron.edu>
2020-11-28 22:48:14 -0500

    resizing text box

--- src/Pages/products.js
+++ src/Pages/products.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import greenhouse from "./../images/greenhouse.jpg";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import TextField from "@material-ui/core/TextField";
+import PropTypes from "prop-types";
 
 const Products = () => {
   const [quantity, setQuantity] = useState("");
@@ -30,21 +31,29 @@ const Products = () => {
         </div>
       </div>
       <div className="product-box">
-        <TextField
-          type="text"
-          className="quantity-input"
-          id="quantity"
-          label="Quantity"
-          variant="outlined"
-          value={quantity}
-          onChange={handleChange}
-        />
-        <h1 className="pounds-total">lbs = Total: </h1>
-        <h1 className="product-output-price"> ${quantity}</h1>
+        <div clasName="product-input">
+          <TextField
+            type="text"
+            className="quantity-input"
+            id="quantity"
+            label="Quantity"
+            variant="outlined"
+            value={quantity}
+            onChange={handleChange}
+          />
+        </div>
+        <h1 className="pounds-total">lbs </h1>
+        <h1 className="pounds-total">=</h1>
+        <h1 className="pounds-total">Total:$</h1>
+        <h1 className="product-output-price"> {quantity}</h1>
         <ShoppingCartIcon className="product-icon" />
       </div>
     </div>
   );
+};
+
+Products.propTypes = {
+  quantity: PropTypes.number,
 };
 
 export default Products;
