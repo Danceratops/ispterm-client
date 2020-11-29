24338e237aa364fda555737b54b7a977a831a84f
sgun7 <srg97@zips.uakron.edu>
2020-11-29 00:55:36 -0500

    Add 404 file

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import search from "./Pages/search";
 import product from "./Pages/product";
 import signout from "./Pages/Signout";
 import thankyou from "./Pages/thankyou";
+import page404 from "./Pages/page404";
 
 import Nav from "./components/navbar";
 
@@ -17,23 +18,24 @@ import Footer from "./components/footer";
 
 function App() {
   return (
-      <div className="App">
-        <Router>
-          <Nav />
-          <Switch>
-            <Route exact path="/" component={home} />
-            <Route exact path="/login" component={login} />
-            <Route exact path="/signup" component={signup} />
-            <Route exact path="/cart" component={cart} />
-            <Route exact path="/products" component={products} />
-            <Route exact path="/search" component={search} />
-            <Route exact path="/signout" component={signout} />
-            <Route exact path="/thankyou" component={thankyou} />
-            <Route exact path="/product/:productId" component={product} />
-          </Switch>
-        </Router>
-        <Footer />
-      </div>
+    <div className="App">
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={home} />
+          <Route exact path="/login" component={login} />
+          <Route exact path="/signup" component={signup} />
+          <Route exact path="/cart" component={cart} />
+          <Route exact path="/products" component={products} />
+          <Route exact path="/search" component={search} />
+          <Route exact path="/signout" component={signout} />
+          <Route exact path="/thankyou" component={thankyou} />
+          <Route exact path="/product/:productId" component={product} />
+          <Route exact path="*" component={page404} />
+        </Switch>
+      </Router>
+      <Footer />
+    </div>
   );
 }
 
